ae8397fe045c284205211a1a5cb8d63ba2feab74
Cem Çiçek <cicekcem25@gmail.com>
2023-04-09 23:31:00 +0300

    day-4

--- Day3/course/modules.py
+++ Day3/course/modules.py
@@ -4,6 +4,7 @@ from main import sayHello
 from human import Human
 #built-in modules
 import random
+from selenium import webdriver
 #package
 
 print(toplamaIslemi(10,20))
@@ -13,4 +14,5 @@ print(random.randint(0,100))
 human1 = Human("Mirza")
 human1.talk("Merhaba")
 
+chromwDriver = webdriver.Chrome()
 #packages
